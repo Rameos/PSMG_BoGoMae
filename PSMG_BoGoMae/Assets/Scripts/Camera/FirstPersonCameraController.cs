1d6750e808af3043021e2d557f8a4553b52fd1d1
nordtanne <ch.maerkl@gmail.com>
2014-07-09 17:58:48 +0200

    merge

--- PSMG_BoGoMae/Assets/Scripts/Camera/FirstPersonCameraController.cs
+++ PSMG_BoGoMae/Assets/Scripts/Camera/FirstPersonCameraController.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using iViewX;
 
 public class FirstPersonCameraController : MonoBehaviour {
 
+    private Vector2 gazePos;
     private float xAxisRotation;
     private float yAxisRotation;
-    private float mouseY;
-    private float mouseX;
+    private float mouseY, y;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    private float mouseX, x;
+    private float rotationY = 0f;
     private float firstPersonLookSpeed = 0.5f;
     private Vector2 firstPersonXAxisClamp = new Vector2(-70.0f, 60.0f);
     private float firstPersonRotationDegreePerSecond = 120f;
@@ -20,16 +28,23 @@ public class FirstPersonCameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        mouseX = Input.GetAxis("Mouse X");
-        mouseY = Input.GetAxis("Mouse Y");
+
+        gazePos = (gazeModel.posGazeLeft + gazeModel.posGazeRight) * 0.5f;
+        x = gazePos.x;
+        y = Screen.height - gazePos.y;
+        Debug.Log(y);
+        //mouseX = Input.GetAxis("Mouse X");
+        //mouseY = Input.GetAxis("Mouse Y");
 	}
 
     void LateUpdate()
     {
-        xAxisRotation += (mouseY * firstPersonLookSpeed);
-        yAxisRotation += (mouseX * firstPersonLookSpeed);
+
+        xAxisRotation = (x );
+        yAxisRotation = (y);
 
         transform.localRotation = Quaternion.Euler(xAxisRotation, yAxisRotation, 0);
+
 
     }
 }
