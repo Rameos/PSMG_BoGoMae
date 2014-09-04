5f0a72c2903098ae5f725667c6053b8e47bbe21c
Gottschalk <florian.gottschalk@stud.uni-regensburg.de>
2014-09-04 14:18:54 +0200

    energy bar increases

--- PSMG_BoGoMae/Assets/Energymanagement.cs
+++ PSMG_BoGoMae/Assets/Energymanagement.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class Energymanagement : MonoBehaviour {
 
-    private float energy = 100;
+    private float energy = 10;
+    private float maxEnergy = 100;
+    private float energyIncreaseFactor = 1;
+
+    private static Texture2D rectTexture;
+    private static GUIStyle rectStyle;
 
 
 	// Use this for initialization
@@ -13,15 +18,40 @@ public class Energymanagement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (energy < maxEnergy)
+        {
+            energy+= energyIncreaseFactor * Time.deltaTime;
+        }
+        
 	}
 
     void OnGUI()
     {
-        GUI.color = Color.green;
-        GUI.backgroundColor = Color.magenta;
+        GUI.color = Color.white;
+        
+        /* Style für Balken
+        if (rectTexture == null)
+        {
+            rectTexture = new Texture2D(1, 1);
+        }
 
-        GUI.Box(new Rect(Screen.width / 2 + 200, Screen.height / 2 - 100, 100, 25), "Energy:" + energy +"/100");
+        if (rectStyle == null)
+        {
+            rectStyle = new GUIStyle();
+        }
+
+        rectTexture.SetPixel(5, 5, Color.grey);
+        rectTexture.Apply();
+
+        rectStyle.normal.background = rectTexture;
+        
+
+        GUI.Box(new Rect(Screen.width / 2 + 200, Screen.height / 2 - 100, 100, 25), "Energy:" + Mathf.Round(energy) +"/"+maxEnergy);
+        GUI.Box(new Rect(Screen.width / 2 + 200, Screen.height / 2 - 100, energy, 25), "" , rectStyle);
+        */
+
+        GUI.Box(new Rect(Screen.width / 2 + 200, Screen.height / 2 - 100, 100, 25), "Energy:" + Mathf.Round(energy) + "/" + maxEnergy);
+        GUI.Box(new Rect(Screen.width / 2 + 200, Screen.height / 2 - 100, energy, 25), "");
     }
 
 
