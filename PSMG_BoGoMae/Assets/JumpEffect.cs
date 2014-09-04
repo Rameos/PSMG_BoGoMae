3c1cdfcdc961b5bf26d7b146775d95c5ebcb5aea
nordtanne <ch.maerkl@gmail.com>
2014-09-04 12:11:28 +0200

    teleport behaviour added

---
+++ PSMG_BoGoMae/Assets/JumpEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("collision ");
+        GameObject refugee = collider.transform.gameObject;
+        refugee.transform.Translate(new Vector3(refugee.transform.position.x + 80, refugee.transform.position.y + 80, refugee.transform.position.z) * 10f * Time.deltaTime, Space.World);
+    }
+
+
+}
