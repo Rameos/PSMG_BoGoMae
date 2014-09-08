45d394d65d06ba10e1b7ff16b3173939f6586cea
nordtanne <ch.maerkl@gmail.com>
2014-09-08 15:35:26 +0200

    network test

---
+++ PSMG_BoGoMae/Assets/RefugeeMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefugeeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float leftRightMovement = Input.GetAxis("Horizontal");
+        float forwardBackwardMovement = Input.GetAxis("Vertical");
+
+        transform.rigidbody.AddForce(leftRightMovement, 0, forwardBackwardMovement, ForceMode.Acceleration);
+	}
+}
