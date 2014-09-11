82e710e6f4975eadf28db55b589db3658b839c98
Gottschalk <florian.gottschalk@stud.uni-regensburg.de>
2014-09-12 00:24:30 +0200

    refugee health

---
+++ PSMG_BoGoMae/Assets/RefugeeHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefugeeHealth : MonoBehaviour {
+
+    private float health = 10;
+    private float maxHealth = 100;
+    private float healthIncreaseFactor = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health < maxHealth)
+        {
+            health += healthIncreaseFactor * Time.deltaTime;
+        }
+	
+	}
+
+    public float Health
+    {
+        get { return health; }
+        set
+        {
+            if (value < 0)
+                health = 0f;
+            else
+            {
+                health = value;
+            }
+        }
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2 - 50, 10, 100, 25), "Health:" + Mathf.Round(health) + "/" + maxHealth);
+        GUI.Box(new Rect(Screen.width / 2 - 50, 10, health, 25), "");
+    }
+}
