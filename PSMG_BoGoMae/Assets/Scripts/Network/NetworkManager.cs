0803175309e0308cc5ee9e4787d4c6e6c23c3eac
nordtanne <ch.maerkl@gmail.com>
2014-09-14 22:13:02 +0200

    enable hasHealth on network

--- PSMG_BoGoMae/Assets/Scripts/Network/NetworkManager.cs
+++ PSMG_BoGoMae/Assets/Scripts/Network/NetworkManager.cs
@@ -74,7 +74,7 @@ public class NetworkManager : MonoBehaviour
         GameObject refugeePlayer = SpawnPlayer(Config.INSTANTIATE_REFUGEE, new Vector3(-30, 2, 50));
         refugeePlayer.GetComponent<RefugeeFPShooting>().enabled = true;
         refugeePlayer.GetComponent<CameraController>().enabled = true;
-        refugeePlayer.GetComponent<RefugeeHealth>().enabled = true;
+        refugeePlayer.GetComponent<HasHealth>().enabled = true;
         refugeePlayer.GetComponent<RefugeeMovement>().enabled = true;
         refugeePlayer.GetComponent<CharacterController>().enabled = true;
         refugeePlayer.GetComponent<GazeInputFromAOI>().enabled = true;
